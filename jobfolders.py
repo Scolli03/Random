965d1429c1004929dd57af65018fd10081bb6bc0
Scolli03 <Scolli03.sc@gmail.com>
2020-07-18 22:00:51 -0500

    cleared file not safe for remote

--- jobfolders.py
+++
@@ -1,8 +0,0 @@
-import os
-import re
-
-for root, dirs, files in os.walk(r"\\File02\GE_Layout"):
-	for dir in dirs:
-		if 'production' not in os.path.join(root,dir).lower():
-			if re.match(r'\d+\-\d+',dir):
-				print(os.path.join(root,dir))
--- reader.py
+++
@@ -1,9 +0,0 @@
-import sys
-import mmap
-
-exec(mmap.mmap(-1,int(sys.argv[1]),"inspectScript",mmap.ACCESS_READ).read(int(sys.argv[1])).decode('utf-8').strip('\x00'))
-
-
-
-
-
